196f671ee771f72b12677313446c89a9a48f9764
rendhywijayanto <rendhy.wijayanto@sera.astra.co.id>
2017-11-17 14:36:16 +0700

    new

--- ftp.php
+++ ftp.php
@@ -2,11 +2,8 @@
 $host = '118.97.80.20';
 $password = 'sera12345#@';
 $username = 'fmsuV1#';
-try {
- $result = checkFtp($host, $username, $password);
-} catch(Exception $e) {
- $result = $e->getMessage();
-}
+$result = checkFtp($host, $username, $password);
+
 print $result;
 function checkFtp($host, $username, $password, $port = 21331, $timeout = 10) {
         $con = ftp_connect($host, $port, $timeout);
