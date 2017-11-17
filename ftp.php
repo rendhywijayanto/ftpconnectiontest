2e00a04c13716ff332f948045f5c1cdcd45f995f
rendhywijayanto <rendhy.wijayanto@sera.astra.co.id>
2017-11-17 14:42:01 +0700

    sd

--- ftp.php
+++ ftp.php
@@ -2,22 +2,23 @@
 $host = '118.97.80.20';
 $password = 'sera12345#@';
 $username = 'fmsuV1#';
-$result = checkFtp($host, $username, $password);
-
-print $result;
-function checkFtp($host, $username, $password, $port = 21331, $timeout = 10) {
+try {
+ $result = checkFtp($host, $username, $password);
+} catch(Exception $e) {
+ $result = $e->getMessage();
+}
+print ($result);
+function checkFtp($host, $username, $password, $port = , $timeout = 10) {
         $con = ftp_connect($host, $port, $timeout);
         if (false === $con) {
-            throw new Exception($e);
-            return "gagal konek";
+            throw new Exception('Unable to connect to FTP Server.');
         }
         $loggedIn = ftp_login($con,  $username,  $password);
         ftp_close($con);
         if (true === $loggedIn) {
             return true;
         } else {
-            throw new Exception($e);
-            return "gagal login"
+            throw new Exception(e);
         }
 }
 
