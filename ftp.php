f800774144dcfdeb59e0c96a9bf168d4c8200bdf
rendhywijayanto <rendhy.wijayanto@sera.astra.co.id>
2017-11-17 15:13:42 +0700

    x

--- ftp.php
+++ ftp.php
@@ -1,24 +1,3 @@
-<?php
-$host = '118.97.80.20';
-$password = 'sera12345#@';
-$username = 'fmsuV1#';
-
-$result = checkFtp($host, $username, $password);
-
-print ($result);
-function checkFtp($host, $username, $password, $port = 990 , $timeout = 10) {
-        $con = ftp_connect($host, $port, $timeout);
-        if (false === $con) {
-            throw new Exception($e);
-            return $e->getMessage();
-        }
-        $loggedIn = ftp_login($con,  $username,  $password);
-        ftp_close($con);
-        if (true === $loggedIn) {
-            return true;
-        } else {
-            throw new Exception(e);
-        }
-}
-
+<?php 
+    echo $_SERVER['SERVER_ADDR']."HAHAHAHAH";
 ?>
